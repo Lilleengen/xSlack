8133bb5e42e93ca4d1dbf8e49b4b8bc7c916fe48
Henrik Lilleengen <mail@ithenrik.com>
2016-11-15 23:08:47 +0000

    Fix edit, break mentions

--- slack.py
+++ slack.py
@@ -11,6 +11,7 @@ def run(token):
     channels = dict()
     clients = list()
     clients_users = dict()
+    clients_channels = dict()
 
     for token_other in config.tokens:
         if token_other != token:
@@ -18,8 +19,13 @@ def run(token):
 
     for client in clients:
         clients_users[client.token] = dict()
+        ts_dict[client.token] = dict()
         for user in client.api_call("users.list")["members"]:
             clients_users[client.token][user["name"]] = user["id"]
+        for channel in client.api_call("channels.list")["channels"]:
+            clients_channels[channel["name"]] = channel["id"]
+        for group in client.api_call("groups.list")["groups"]:
+            clients_channels[group["name"]] = group["id"]
 
     sc = SlackClient(token)
 
@@ -37,25 +43,24 @@ def run(token):
         while True:
             for action in sc.rtm_read():
                 print(action)
-                if "channel" in action:
-                    print(action["channel"])
-                    print(channels)
                 if "type" in action and ( action["type"] == "team_join" or action["type"] == "user_change"):
                     members[action["user"]["id"]] = dict()
                     members[action["user"]["id"]]["name"] = action["user"]["name"]
                     members[action["user"]["id"]]["image"] = action["user"]["profile"]["image_32"]
-                elif "channel" in action and action["channel"] in config.channels and "type" in action and action["type"] == "message" and "subtype" in action and action["subtype"] == "message_changed" and action["message"]["ts"] in ts_dict:
-                    text = re.sub(r"<@U(?:\d|\w){8}>", lambda m: "@" + members[m.group(0)[2:-1]]["name"], action["message"]["text"])
-                    for token_other in config.tokens:
-                        if token_other != token:
-                            sc.api_call("chat.update", token=token_other, ts=ts_dict[action["message"]["ts"]], channel=action["channel"], text=text)
+                elif "channel" in action and action["channel"] in channels and channels[action["channel"]] in config.channels and action["type"] == "message" and "subtype" in action and action["subtype"] == "message_changed":
+                    text = re.sub(r"<@U(?:\d|\w){8}>", lambda m: "-----@-----" + members[m.group(0)[2:-1]]["name"], action["message"]["text"])
+                    for client in clients:
+                        if action["message"]["ts"] in ts_dict[client.token]:
+                            text = re.sub(r"(?:^| |\n)@(?:\d|[a-z]){1,23}(?:$| |\n)", lambda m: re.sub(r"@(?:\d|[a-z]){1,23}", lambda m2: m2.group(0) if m2.group(0) not in clients_users[client.token] else "<@" + clients_users[client.token][m2.group(0)[1:]] + ">", m.group(0)), text)
+                            text = re.sub(r"-----@-----", "@", text)
+                            print(client.api_call("chat.update", ts=ts_dict[client.token][action["message"]["ts"]], channel=clients_channels[channels[action["channel"]]], text=text))
                 elif "channel" in action and action["channel"] in channels and channels[action["channel"]] in config.channels and "type" in action and action["type"] == "message" and "text" in action and action["text"] != last_text and "user" in action:
                     last_text=action["text"]
-                    text = re.sub(r"(?:^| |\n)@(?:\d|[a-z]){1,23}(?:$| |\n)", lambda m: re.sub(r"@(?:\d|[a-z]){1,23}", lambda m2: "<-----@" + clients_users[client.token][m2.group(0)[1:]] + ">", m.group(0)), action["text"])
-                    text = re.sub(r"<@U(?:\d|\w){8}>", lambda m: "@" + members[m.group(0)[2:-1]]["name"], text)
-                    text = re.sub(r"<-----@", "<@", text)
+                    text = re.sub(r"<@U(?:\d|\w){8}>", lambda m: "-----@-----" + members[m.group(0)[2:-1]]["name"], action["text"])
                     for client in clients:
-                        ts_dict[action["ts"]] = client.api_call("chat.postMessage", channel=channels[action["channel"]], text=text, username=members[action["user"]]["name"] + " @ " + team_name, icon_url=members[action["user"]]["image"])["ts"]
+                        text = re.sub(r"(?:^| |\n)@(?:\d|[a-z]){1,23}(?:$| |\n)", lambda m: re.sub(r"@(?:\d|[a-z]){1,23}", lambda m2: m2.group(0) if m2.group(0) not in clients_users[client.token] else "<@" + clients_users[client.token][m2.group(0)[1:]] + ">", m.group(0)), text)
+                        text = re.sub(r"-----@-----", "@", text)
+                        ts_dict[client.token][action["ts"]] = client.api_call("chat.postMessage", channel=clients_channels[channels[action["channel"]]], text=text, username=members[action["user"]]["name"] + " @ " + team_name, icon_url=members[action["user"]]["image"])["ts"]
             time.sleep(0.1)
     else:
         print('Connection Failed, invalid token?')
