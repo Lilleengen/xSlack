7514f2c00fd96c7e0e7f41d9057d5a1b722b9d6b
Henrik Lilleengen <mail@ithenrik.com>
2016-11-22 20:40:24 +0000

    Misc

--- setup.py
+++ setup.py
@@ -18,12 +18,12 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='xslack-deamon',
+    name='xSlack',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',
+    version='0.2',
 
     description='Open source, self hosted cross-team channels for Slack!',
     long_description=long_description,
